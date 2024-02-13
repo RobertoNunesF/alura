2d4df74e11f9d483a357bcdf9f397559a02db804
Roberto Nunes Ferreira <rrobertonunesferreira@gmail.com>
2024-02-13 02:21:01 -0300

    finalizando jogo

--- Front-End/JavaScript/LogicaProgramacao/app.js
+++ Front-End/JavaScript/LogicaProgramacao/app.js
@@ -1,24 +1,32 @@
 alert('Boas vindas ao jogo do número secreto');
 
-let numeroSecreto = 4;
+let numeroMaximo = 100;
+
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 
 let chute; 
 
+let tentativas = 1;
+
 
 // enquanto o chute não foi igual ao n.s.
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo} `);
 
     // se o chute for igual ao numero secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+        break;
     } else {
         if (chute > numeroSecreto ) {
             alert(`O número secreto é menor que o ${chute}`);
         } else {
             alert(`O número secreto é maior que o ${chute}`);
         }
+        tentativas++;
     }
-
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
