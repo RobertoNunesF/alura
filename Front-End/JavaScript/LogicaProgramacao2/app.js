1521548f56716d463fef77a95115e127a29ef151
Roberto Nunes Ferreira <rrobertonunesferreira@gmail.com>
2024-02-13 23:21:48 -0300

    iniciando curso

---
+++ Front-End/JavaScript/LogicaProgramacao2/app.js
@@ -0,0 +1,18 @@
+let numeroSecreto = gerarNumero();
+
+function exibirTexto (tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTexto('h1', 'Jogo do número secreto');
+exibirTexto('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+
+function gerarNumero() {
+    return parseInt(Math.random() * 10 + 1);
+}
